b93498eea197f085026c883c026764682b69f660
tumkir <tumkir@gmail.com>
2020-12-13 22:57:01 +0300

    Add field called_at, delivered_at, registrated_at for Order model

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 
 class Restaurant(models.Model):
@@ -79,6 +80,9 @@ class Order(models.Model):
     address = models.CharField(max_length=500, verbose_name='Адрес')
     status = models.CharField(max_length=15, choices=ORDER_STATUS_CHOICES, default='NEW', verbose_name='Статус заказа')
     comment = models.TextField(blank=True, verbose_name='Комментарий')
+    registrated_at = models.DateTimeField(default=timezone.now, verbose_name='Зарегистрирован в')
+    called_at = models.DateTimeField(null=True, blank=True, verbose_name='Позвонили в')
+    delivered_at = models.DateTimeField(null=True, blank=True, verbose_name='Доставлен в')
 
     class Meta:
         verbose_name = 'заказ'
